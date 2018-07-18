c894c03cadbddde1a257a657db6d33927726a1ad
joy <joyee@coupang.com>
2018-07-18 13:39:29 +0900

    회의실 예약현황 조회 화면 수정

--- src/main/resources/static/js/schedule/status.js
+++ src/main/resources/static/js/schedule/status.js
@@ -1,19 +1,28 @@
 $(function () {
-  Date.prototype.yyyyMMdd = function()
-  {
+  Date.prototype.yyyyMMdd = function () {
     var yyyy = this.getFullYear().toString();
     var mm = (this.getMonth() + 1).toString();
     var dd = this.getDate().toString();
 
-    return yyyy + (mm[1] ? mm : '0'+mm[0]) + (dd[1] ? dd : '0'+dd[0]);
+    return yyyy + (mm[1] ? mm : '0' + mm[0]) + (dd[1] ? dd : '0' + dd[0]);
   };
+
+  $('input[data-date-picker="on"]').datepicker('option', 'dateFormat', 'yy-mm-dd');
+
   var scheduleDate = $('#scheduleDate').val();
   if (util.isEmpty(scheduleDate)) {
     scheduleDate = (new Date()).yyyyMMdd();
   }
+  var formatDate = scheduleDate.substring(0, 4) + '-' + scheduleDate.substring(4, 6) + '-' + scheduleDate.substring(6, 8);
+  $('#scheduleDateInput').val(formatDate);
+  $('#scheduleDateInput').on('change', function () {
+    var searchDate = $(this).val();
+    location.href = '/schedule/list/' + searchDate.replace(/-/gi, '')
+  });
   $('#calendar').fullCalendar({
     defaultView: 'agendaDay',
     defaultDate: scheduleDate,
+    header: false,
     events: {
       url: '/schedule/get',
       type: 'GET',
