2f20d76ced7b6a8adfe937e67e83ca0eae3fc2ab
joy <joyee@coupang.com>
2018-07-23 19:40:01 +0900

    코드 정리

--- src/main/java/com/schedule/repository/ConferenceRepositoryImpl.java
+++ src/main/java/com/schedule/repository/ConferenceRepositoryImpl.java
@@ -3,7 +3,6 @@ package com.schedule.repository;
 import com.schedule.repository.entity.Conference;
 import com.schedule.repository.exception.TransactionManagerException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.*;
 
@@ -11,7 +10,6 @@ public class ConferenceRepositoryImpl implements ConferenceRepositoryCustom {
 	@Autowired
 	private EntityManagerFactory emf;
 
-	@Transactional
 	@Override
 	public void makeConferenceBookedStatus(Long conferenceId, String conferenceTitle, Long memberId) {
 		EntityManager em = emf.createEntityManager();
